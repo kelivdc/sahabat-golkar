4aea8d62a92de74e62753867a10e08c25094b0bf
Arie <kelivdc@yahoo.com>
2023-03-05 21:58:22 +0700

    Add Logo

--- app/page.js
+++ app/page.js
@@ -6,18 +6,11 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className="center">
-      <div className="max-w-screen-lg m-auto border-4 md:border-8 border-red-500 bg-blue-400 min-h-[92vh] md:min-h-[99vh]">
-        <div className="bg-yellow-400 rounded-[50px] md:rounded-[105px] min-h-[91vh] md:min-h-[97vh] p-8 text-gray-600">
-          Eiusmod non occaecat exercitation irure excepteur exercitation dolore
-          pariatur ipsum. Duis sint consectetur nisi quis nulla enim nisi est
-          amet eiusmod aute sit. Est ad officia minim deserunt labore est sint.
-          Non veniam eu officia ad ea enim et. Ipsum sint ea anim irure ut
-          excepteur deserunt reprehenderit excepteur mollit ipsum. Tempor aute
-          proident duis ea id consectetur proident pariatur fugiat qui amet ex
-          aliqua. Voluptate anim et mollit esse nulla incididunt. Culpa id minim
-          sint ut minim deserunt cupidatat magna amet tempor cillum sit. Veniam
-          consequat veniam aliquip velit. Nisi consectetur excepteur aliqua ad
-          laboris nostrud quis ut magna tempor quis nisi occaecat. 
+      <div className="max-w-screen md:max-w-[60vw] m-auto border-4 md:border-8 border-red-500 bg-blue-400 min-h-[92vh] md:min-h-[99vh]">
+        <div className="bg-[#ffcd01] rounded-[50px] md:rounded-[105px] min-h-[91vh] md:min-h-[97vh] text-gray-600 m-auto grid place-items-center">
+          <div className="relative w-full md:w-[95%] h-60 md:h-[80vh]">
+            <Image src="/logo.png" fill sizes="(width: 768px) 100vw, (max-width: 1800vw) 70vw, 33vw" alt="Logo" />
+          </div>
         </div>
       </div>      
     </div>
