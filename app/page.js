9da834d8d6ba91a3ef0f3f158ca04a5c460f8209
Arie <kelivdc@yahoo.com>
2023-03-07 17:45:17 +0700

    Update

--- app/page.js
+++ app/page.js
@@ -4,19 +4,33 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sahabat Golkar"
-}
+  title: "Sahabat Golkar",
+};
 
 export default function Home() {
   return (
-    <div className="center">
-      <div className="max-w-screen md:max-w-[60vw] m-auto border-4 md:border-8 border-red-500 bg-blue-400 min-h-[92vh] md:min-h-[99vh]">
-        <div className="bg-[#ffcd01] rounded-[50px] md:rounded-[105px] min-h-[91vh] md:min-h-[97vh] text-gray-600 m-auto grid place-items-center">
-          <div className="relative w-full md:w-[95%] h-60 md:h-[80vh]">
-            <Image src="/logo.png" fill sizes="(width: 768px) 100vw, (max-width: 1800vw) 70vw, 33vw" alt="Logo" />
+    <>
+      <div className="center">
+        <div className="max-w-screen md:max-w-[65vw] m-auto min-h-[92vh] md:min-h-[99vh] border-4 ">
+          <div className="rounded-[50px] md:rounded-[105px] min-h-[91vh] md:min-h-[97vh] text-gray-600 m-auto grid place-items-center">
+            <div className="relative w-full h-72 md:h-[100vh]">
+              <Image
+                src="/1.png"
+                fill
+                
+                alt="Logo"
+              />
+            </div>
           </div>
-        </div>
-      </div>      
-    </div>
+          <div className="relative w-full h-72 md:h-[100vh]">
+          <Image
+                src="/2.png"
+                fill                
+                alt="Mari Berkarya"
+              />
+          </div>
+        </div>        
+      </div>
+    </>
   );
 }
