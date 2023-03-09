70ae0c3f99b076498b3d5340d3bce26ed0a68fc7
Arie <kelivdc@yahoo.com>
2023-03-09 20:50:15 +0700

    Update bantuan

--- app/penerima/page.js
+++ app/penerima/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import HeaderNav from "@/components/HeaderNav";
+import Contoh from "@/components/Contoh";
 
 export const metadata = {
   title: "Penerima Bantuan UMKM",
@@ -9,96 +10,28 @@ export const metadata = {
 
 function Penerima() {
   return (
-    <>
-      <div className="max-w-screen md:max-w-[65vw] m-auto min-h-[92vh] md:min-h-[99vh] relative bg-gradient-to-b from-white to-yellow-500 p-4">
-        <HeaderNav />
-        <div className="flex justify-between">
-          <div className="relative w-1/2 md:w-[30%] h-48">
-            <Image src="/logo.png" alt="Logo" fill />
-          </div>
-          <div className="w-1/2 md:w-[70%] px-4 md:px-10 m-auto">
-            <h1 className="text-xl md:text-[40px] md:font-bold leading-10">
-              DATA PENERIMA
-              <br />
-              BANTUAN UMKM
+    <>      
+       <div className="flex flex-col max-w-screen md:max-w-[65vw] m-auto min-h-screen relative max-h-screen bg-gradient-to-b from-white to-yellow-500 px-4 pt-4">
+      <div className="flex justify-between">
+        <div className="w-1/3">
+          <Image src="/logo.png" width={270} height={270} alt="Logo" />
+        </div>
+        <div className="w-2/3">
+          <div className="grid content-center h-full relative">
+            <div className="absolute top-0 right-0 flex">
+              <Link href="/"><div className="hover:bg-green-400 hover:rounded-md px-2 py-1">Home</div></Link>
+              <Link href="/bantuan"><div className="hover:bg-green-400 hover:rounded-md px-2 py-1">Bantuan dan Pelayanan</div></Link>
+            </div>
+            <h1 className="text-xl md:text-[40px] md:font-bold text-center m-auto uppercase leading-relaxed">
+              Data penerima<br />Bantuan UMKM
             </h1>
           </div>
         </div>
-        <div className="mt-4">
-          <table className="w-full border-2 border-white" cellPadding="2" cellSpacing="2">
-            <thead className="bg-blue-400">
-              <tr>
-                <th width="5%">No</th>
-                <th>Nama</th>
-                <th>Kelurahan</th>
-                <th>Photo</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="rowColor">
-                <td className="tcell text-right">1</td>
-                <td className="tcell">Alex Sanusi</td>
-                <td className="tcell">Desa Kuningan</td>
-                <td className="tcell">
-                  <div className="relative rounded-full w-10 h-10 m-auto">
-                  <Image src="/bantuan.jpg" fill alt="Bantuan" />
-                  </div>
-                </td>
-              </tr>
-              <tr className="rowColor">
-                <td className="tcell text-right">2</td>
-                <td className="tcell">Sutarji</td>
-                <td className="tcell">Desa Kuningan</td>
-                <td className="tcell">
-                  <div className="relative rounded-full w-10 h-10 m-auto">
-                  <Image src="/bantuan.jpg" fill alt="Bantuan" />
-                  </div>
-                </td>
-              </tr>
-              <tr className="rowColor">
-                <td className="tcell text-right">3</td>
-                <td className="tcell">Dansir</td>
-                <td className="tcell">Desa Kuningan</td>
-                <td className="tcell">
-                  <div className="relative rounded-full w-10 h-10 m-auto">
-                  <Image src="/bantuan.jpg" fill alt="Bantuan" />
-                  </div>
-                </td>
-              </tr>
-              <tr className="rowColor">
-                <td className="tcell text-right">4</td>
-                <td className="tcell">Walijah</td>
-                <td className="tcell">Desa Kuningan</td>
-                <td className="tcell">
-                  <div className="relative rounded-full w-10 h-10 m-auto">
-                  <Image src="/bantuan.jpg" fill alt="Bantuan" />
-                  </div>
-                </td>
-              </tr>
-              <tr className="rowColor">
-                <td className="tcell text-right">5</td>
-                <td className="tcell">Karto</td>
-                <td className="tcell">Desa Kuningan</td>
-                <td className="tcell">
-                  <div className="relative rounded-full w-10 h-10 m-auto">
-                  <Image src="/bantuan.jpg" fill alt="Bantuan" />
-                  </div>
-                </td>
-              </tr>
-              <tr className="rowColor">
-                <td className="tcell text-right">6</td>
-                <td className="tcell">Walangsi</td>
-                <td className="tcell">Desa Kuningan</td>
-                <td className="tcell">
-                  <div className="relative rounded-full w-10 h-10 m-auto">
-                  <Image src="/bantuan.jpg" fill alt="Bantuan" />
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
       </div>
+      <div className="grid align-middle place-items-center flex-1">
+        <Contoh />
+      </div>
+    </div>
     </>
   );
 }
--- components/Contoh.js
+++ components/Contoh.js
@@ -1,35 +1,9 @@
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
-import HeaderNav from "@/components/HeaderNav";
-import Contoh from "@/components/Contoh";
 
-export const metadata = {
-  title: "Data Penerima Bantuan Sekolah",
-};
-
-function Penerima() {
+function Contoh() {
   return (
-    <>
-       <div className="flex flex-col max-w-screen md:max-w-[65vw] m-auto min-h-screen relative max-h-screen bg-gradient-to-b from-white to-yellow-500 px-4 pt-4">
-      <div className="flex justify-between">
-        <div className="w-1/3">
-          <Image src="/logo.png" width={270} height={270} alt="Logo" />
-        </div>
-        <div className="w-2/3">
-          <div className="grid content-center h-full relative">
-            <div className="absolute top-0 right-0 flex">
-              <Link href="/"><div className="hover:bg-green-400 hover:rounded-md px-2 py-1">Home</div></Link>
-              <Link href="/bantuan"><div className="hover:bg-green-400 hover:rounded-md px-2 py-1">Bantuan dan Pelayanan</div></Link>
-            </div>
-            <h1 className="text-xl md:text-[40px] md:font-bold text-center m-auto uppercase leading-relaxed">
-              Data penerima<br />Bantuan Sekolah
-            </h1>
-          </div>
-        </div>
-      </div>
-      <div className="grid align-middle place-items-center flex-1">
-      <table
+    <table
       className="w-full border-2 border-white"
       cellPadding="2"
       cellSpacing="2"
@@ -37,10 +11,10 @@ function Penerima() {
       <thead className="bg-blue-400">
         <tr>
           <th width="5%">No</th>
-          <th>Nama Orang Tua</th>
-          <th>Nama Siswa</th>
+          <th>Nama</th>
+          <th>NIK</th>
           <th>Alamat</th>
-          <th>Asal Sekolah</th>
+          <th>Jenis Usaha</th>
           <th>Photo Usaha</th>
         </tr>
       </thead>
@@ -48,9 +22,9 @@ function Penerima() {
         <tr className="rowColor">
           <td className="tcell text-right">1</td>
           <td className="tcell">Januar</td>
-          <td className="tcell">Pratiwi</td>
+          <td className="tcell">1234456567687</td>
           <td className="tcell">Jl. Bahari 9 no.12</td>
-          <td className="tcell">SDN Pucung 9</td>
+          <td className="tcell">Bengkel Sepeda</td>
           <td className="tcell">
             <div className="relative rounded-full w-10 h-10 m-auto">
               <Image src="/bantuan.jpg" fill alt="Bantuan" />
@@ -60,9 +34,9 @@ function Penerima() {
         <tr className="rowColor">
           <td className="tcell text-right">2</td>
           <td className="tcell">Mulyati</td>
-          <td className="tcell">Surya Purnomo</td>
+          <td className="tcell">1234456567687</td>
           <td className="tcell">Jl. Siliwangi III no.60 </td>
-          <td className="tcell">SMP N 13</td>
+          <td className="tcell">Toko Kueh</td>
           <td className="tcell">
             <div className="relative rounded-full w-10 h-10 m-auto">
               <Image src="/bantuan.jpg" fill alt="Bantuan" />
@@ -72,9 +46,9 @@ function Penerima() {
         <tr className="rowColor">
           <td className="tcell text-right">3</td>
           <td className="tcell">Wisnu</td>
-          <td className="tcell">Khasim Yuda</td>
+          <td className="tcell">1234456567687</td>
           <td className="tcell">Komp. Perdamaian Blok IX</td>
-          <td className="tcell">SDN Pagi 9</td>
+          <td className="tcell">TK Islam</td>
           <td className="tcell">
             <div className="relative rounded-full w-10 h-10 m-auto">
               <Image src="/bantuan.jpg" fill alt="Bantuan" />
@@ -84,9 +58,9 @@ function Penerima() {
         <tr className="rowColor">
           <td className="tcell text-right">4</td>
           <td className="tcell">Pramudi</td>
-          <td className="tcell">Suci Mentari</td>
+          <td className="tcell">1234456567687</td>
           <td className="tcell">Jl. Wisata/IV no.20</td>
-          <td className="tcell">SDN 10</td>
+          <td className="tcell">Toko Sembako</td>
           <td className="tcell">
             <div className="relative rounded-full w-10 h-10 m-auto">
               <Image src="/bantuan.jpg" fill alt="Bantuan" />
@@ -96,9 +70,9 @@ function Penerima() {
         <tr className="rowColor">
           <td className="tcell text-right">5</td>
           <td className="tcell">Hilman</td>
-          <td className="tcell">Khadijah</td>
+          <td className="tcell">1234456567687</td>
           <td className="tcell">Jl. Belawan I no.25</td>
-          <td className="tcell">SDN 34</td>
+          <td className="tcell">Toko Sembako</td>
           <td className="tcell">
             <div className="relative rounded-full w-10 h-10 m-auto">
               <Image src="/bantuan.jpg" fill alt="Bantuan" />
@@ -107,10 +81,7 @@ function Penerima() {
         </tr>
       </tbody>
     </table>
-      </div>
-    </div>
-    </>
   );
 }
 
-export default Penerima;
+export default Contoh;
